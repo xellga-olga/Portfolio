63a24006c5fabfe503292219ee3ab30e09c7f98a
Noskina Olha <noskinaolga1@gmail.com>
2024-09-20 16:54:35 +0200

    Skills component

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -4,7 +4,7 @@ import styles from "./Projects.module.css";
 
 const Projects = () => {
   return <section className={styles.container} id="projects">
-    <h1 className={styles.sectionTitle}>Projects</h1>
+    <h1 className='sectionTitle'>Projects</h1>
     <div className={styles.projectContainer}>
      <ProjectCard />
     </div>
